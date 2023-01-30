2334c147fe4af6c80880955a48a18ae198301924
Harsh Shandilya <me@msfjarvis.dev>
2023-01-30 16:24:49 +0530

    refactor: migrate to Nix Flakes

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs = {url = "github:NixOS/nixpkgs/nixpkgs-unstable";};
+    flake-utils = {url = "github:numtide/flake-utils";};
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          git
+          go_1_19
+          gopls
+        ];
+      };
+    });
+}
