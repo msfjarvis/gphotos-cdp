7a84051b784290911cf6ddab1349067f15afeac8
Harsh Shandilya <me@msfjarvis.dev>
2023-02-12 19:39:30 +0530

    feat: set up gomod2nix

--- flake.nix
+++ flake.nix
@@ -6,22 +6,38 @@
       url = "github:edolstra/flake-compat";
       flake = false;
     };
+    go2nix = {
+      url = "github:nix-community/gomod2nix/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.utils.follows = "flake-utils";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    go2nix,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [go2nix.overlays.default];
+      };
     in {
+      packages.default = pkgs.buildGoApplication {
+        pname = "gphotos-cdp";
+        version = "1.0.0";
+        pwd = ./.;
+        src = ./.;
+        modules = ./gomod2nix.toml;
+      };
       devShells.default = pkgs.mkShell {
         nativeBuildInputs = with pkgs; [
           git
-          go_1_19
-          gopls
+          gomod2nix
+          (mkGoEnv {pwd = ./.;})
         ];
       };
     });
